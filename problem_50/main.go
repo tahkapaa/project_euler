5cfd4fe835ee628ff5a59cfc305d0a3eca5449a6
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-01-15 02:31:20 +0200

    Brute forcing

--- problem_50/main.go
+++ problem_50/main.go
@@ -10,19 +10,47 @@ The longest sum of consecutive primes below one-thousand that adds to a prime, c
 Which prime, below one-million, can be written as the sum of the most consecutive primes?
 */
 
-func main() {
-	fmt.Println(getPrimesUnder(100))
+type primes []int
+type solutions []primes
+
+func (p primes) sum() int {
+	sum := 0
+	for _, val := range p {
+		sum += val
+	}
+	return sum
 }
 
-func getPrimesUnder(n int) []int {
-	var primes []int
-	for i := 0; i < n; i++ {
-		if isPrime(i) {
-			fmt.Println(i)
-			primes = append(primes, i)
+func main() {
+	p := getPrimesUnder(1000)
+	fmt.Println(p)
+
+	for i := 0; i < len(p); i++ {
+		fmt.Println("Finding target: ", p[i])
+		var s solutions
+		ok := getSum(p[:i], primes{}, p[i], &s)
+		fmt.Printf("Finding: %d, Success: %v, Count(Solutions): %v\n", p[i], ok, len(s))
+		printLongestSolution(s)
+	}
+}
+func printLongestSolution(s solutions) {
+	var maxPrimes primes
+	for _, p := range s {
+		if len(p) > len(maxPrimes) {
+			maxPrimes = p
 		}
 	}
-	return primes
+	fmt.Printf("Max primes length: %d, Example: %v\n", len(maxPrimes), maxPrimes)
+}
+
+func getPrimesUnder(n int) primes {
+	var p primes
+	for i := 0; i < n; i++ {
+		if isPrime(i) {
+			p = append(p, i)
+		}
+	}
+	return p
 }
 
 func isPrime(n int) bool {
@@ -35,4 +63,21 @@ func isPrime(n int) bool {
 		}
 	}
 	return true
+}
+
+func getSum(p primes, sumParts primes, target int, sol *solutions) bool {
+	for i := len(p) - 1; i >= 0; i-- {
+		if p[i]+sumParts.sum() < target {
+			newParts := append(sumParts, p[i])
+			if i > 0 {
+				getSum(p[:i], newParts, target, sol)
+			}
+		} else if p[i]+sumParts.sum() == target {
+			// fmt.Println("***** FOUND SUM *****")
+			newSolution := append(sumParts, p[i])
+			*sol = append(*sol, newSolution)
+			return true
+		}
+	}
+	return len(*sol) > 0
 }
